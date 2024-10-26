19e5bdbb8ac9b068b316aacbb054e683ca5fc4f2
shahriaarrr <shahriaarrr@gmail.com>
2024-10-26 21:03:25 +0330

    add basic hello world route

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import express from "express";
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+
 app.listen(8000, () => {console.log("server started")});
