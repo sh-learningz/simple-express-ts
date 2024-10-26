97ca98ce090052327c4416e79580dcbcbc888294
shahriaarrr <shahriaarrr@gmail.com>
2024-10-26 21:16:50 +0330

    parse request body

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import reminderRouter from "./routers/reminder";
 
 const app = express();
 
-app.use('/reminders', reminderRouter)
+app.use(express.json());
+app.use('/reminders', reminderRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
--- routers/reminder.ts
+++ routers/reminder.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import createReminderDTO from "../dtos/create-reminder";
 
 const router = Router();
 
 router.get('/', (req, res) => {
     res.send('List of reminders');
+})
+
+router.post('/', (req, res) => {
+    const {title} = req.body as createReminderDTO
 })
 
 export default router;
